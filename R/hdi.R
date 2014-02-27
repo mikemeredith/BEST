7cd6d735392df8f2be74edb765c240354081dc04
Mike Meredith <mmeredith@wcsmsia>
2014-02-27 11:37:56 +0800

    Fixed hdi.density

--- R/hdi.R
+++ R/hdi.R
@@ -65,23 +65,34 @@ hdi.function <- function(object, credMass=0.95, tol, ...)  {
 }
 
 
-hdi.density <- function(object, credMass=0.95, combine=TRUE, ...) {
+hdi.density <- function(object, credMass=0.95, allowSplit=FALSE, ...) {
   if(is.na(credMass) || length(credMass) != 1 || credMass <= 0 || credMass >= 1)
     stop("credMass must be in 0 < credMass < 1")
   sorted = sort( object$y , decreasing=TRUE )
   heightIdx = min( which( cumsum( sorted) >= sum(object$y) * credMass ) )
   height = sorted[heightIdx]
   indices = which( object$y >= height )
-  HDImass = sum( object$y[indices] ) / sum(object$y)
+  # HDImass = sum( object$y[indices] ) / sum(object$y)
   gaps <- which(diff(indices) > 1)
-  if(length(gaps) > 0 && combine)
-    warning("The HDI is discontinuous.")
-  begs <- indices[c(1, gaps + 1)]
-  ends <- indices[c(gaps, length(indices))]
-  result <- cbind(begin = object$x[begs], end = object$x[ends])
-  if(combine)  {
-    result <- range(result)
-    names(result) <- c("lower", "upper")
+  if(length(gaps) > 0 && !allowSplit) {
+    # In this case, return shortest 95% CrI
+    warning("The HDI is discontinuous but allowSplit = FALSE;
+    the result is a valid CrI but not HDI.")
+    cumul <- cumsum(object$y) / sum(object$y)
+    upp.poss <- low.poss <- which(cumul < 1 - credMass)
+    for (i in low.poss)
+      upp.poss[i] <- min(which(cumul > cumul[i] + credMass))
+    # all(cumul[upp.poss] - cumul[low.poss] > credMass) # check
+    width <- upp.poss - low.poss
+    best <- which(width == min(width))  # usually > 1 value due to ties
+    result <- c(lower = mean(object$x[low.poss[best]]),
+                upper = mean(object$x[upp.poss[best]]))
+  } else {
+    begs <- indices[c(1, gaps + 1)]
+    ends <- indices[c(gaps, length(indices))]
+    result <- cbind(begin = object$x[begs], end = object$x[ends])
+    if(!allowSplit)
+      names(result) <- c("lower", "upper")
   }
   attr(result, "credMass") <- credMass
   attr(result, "height") <- height
